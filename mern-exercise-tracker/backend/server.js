902769129bdc139887aeac13b8ba0330bda735c8
MohamadDibassy <Mohamaddibassy14@gmail.com>
2023-10-28 12:20:51 -0400

    feat:add  user schema; add exercise schema; add server api endpoints

--- mern-exercise-tracker/backend/server.js
+++ mern-exercise-tracker/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
@@ -8,7 +9,6 @@ const app = express();
 const uri = process.env.ATLAS_URI;
  //mongoose.connect(uri, { useNewUrlParser: false, useCreateIndex: false }
  //);
-//mongoose.connect(uri, { useNewUrlParser: false, useCreateIndex: false });
 mongoose.connect(uri, {});
 const connection = mongoose.connection;
 connection.once('open', () => {
@@ -18,6 +18,25 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', (req, res) => {
+  console.log(req.body);
+  res.send(
+    `I received your POST request. This is what you sent me: ${req.body.post}`,
+  );
+});
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
